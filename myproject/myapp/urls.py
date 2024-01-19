e207dec42a6459cf40d3d22a0d7706a508afdb0a
Adi Malca <adimalca91@gmail.com>
2024-01-19 16:55:46 +0200

    Added Body Parameters

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('getuser/<name>/<id>', views.pathview, name='pathview'), # Path Parameters
-    path('getuser/', views.qryview, name='qryview'), # Query String Parameters
+    path('getuser/', views.qryview, name='qryview'), # Query String Parameters - GET requests
+    path('showform/', views.showform, name='showform'), # Body Parameters - POST requests
+    path('myapp/getform/', views.getform, name='getform'), # Body Parameters - POST requests
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def home(request):
     return HttpResponse("Welcome to the Little Lemon!")
@@ -9,11 +10,18 @@ def home(request):
 def pathview(request, name, id):
     return HttpResponse(f"Path Parameters: Name: {name}, User ID: {id}")
 
-# Query String Parameters
+# Query String Parameters - for GET requests
 def qryview(request):
     name = request.GET['name']
     id = request.GET['id']
     return HttpResponse(f"Query String Parameters: Name: {name}, User ID: {id}")
 
+# Body Parameters - for POST requests
+def showform(request):
+    return render(request, "bodyparamform.html")
 
-    
+def getform(request):
+    if request.method == "POST":
+        id = request.POST['userid']
+        name = request.POST['name']
+    return HttpResponse(f"Body Parameters: Name: {name}, User ID: {id}")
