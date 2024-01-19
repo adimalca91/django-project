f433338607f5c878193b2b6467dc1d5e75c707db
Adi Malca <adimalca91@gmail.com>
2024-01-19 15:59:53 +0200

    Added Path Parameters

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('getuser/<name>/<id>', views.pathview, name='pathview'),
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -4,3 +4,10 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse("Welcome to the Little Lemon!")
+
+# Path Parameters
+def pathview(request, name, id):
+    return HttpResponse(f"Name: {name}, User ID: {id}")
+
+
+    
