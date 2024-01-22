0734def01576d681791b8cb65460a83a6cb2a542
Adi Malca <adimalca91@gmail.com>
2024-01-22 11:27:09 +0200

    Creating a Model and working with migrations

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+'''
+This is a model that will be used as a database for storing the different
+menu items that are being displayed on the Little Lemon website.
+'''
+class Drinks(models.Model):
+    # Django will create a default primary key called "id"
+    drink_name = models.CharField(max_length=200)
+    price = models.IntegerField()
