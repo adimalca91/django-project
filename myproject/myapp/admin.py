7a13df8e819dcbf6af19b20cbd4a4d5ac834c3bf
Adi Malca <adimalca91@gmail.com>
2024-01-22 15:55:27 +0200

    Connecting 2 models with foreign key

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Drinks
+from .models import Drinks, DrinksCategory
 
 # Register your models here.
 
 admin.site.register(Drinks)
+admin.site.register(DrinksCategory)
--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -3,10 +3,20 @@ from django.db import models
 # Create your models here.
 
 '''
+Here there are 2 models which are connected via one-to-many relationship because
+many drinks can belong to one category.
+'''
+
+class DrinksCategory(models.Model):
+    category_name = models.CharField(max_length=200)
+
+
+'''
 This is a model that will be used as a database for storing the different
 menu items that are being displayed on the Little Lemon website.
 '''
 class Drinks(models.Model):
     # Django will create a default primary key called "id"
-    drink_name = models.CharField(max_length=200)
+    drink = models.CharField(max_length=200)
     price = models.IntegerField()
+    category_id = models.ForeignKey(DrinksCategory, on_delete=models.PROTECT, default=None)
